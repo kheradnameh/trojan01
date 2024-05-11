b77c90c380a2558dea3ea607df0fb6435d530424
Ali Ghadrboland <37036008+alighadrboland@users.noreply.github.com>
2024-05-11 11:00:35 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,8 +1,7 @@
 // src/worker.js
 import { connect } from "cloudflare:sockets";
 let sha224Password = '156957625a7b5066506652d122fc157d667ba7ba8f6963ca2af71819';
-const proxyIPs = ['proxy.alighadrboland.ir','proxyy.alighadrboland.ir','proxyyy.alighadrboland.ir','proxyyyy.alighadrboland.ir','proxyyyyy.alighadrboland.ir']; // const proxyIPs = ['cdn-all.xn--b6gac.eu.org', 'cdn.xn--b6gac.eu.org', 'cdn-b100.xn--b6gac.eu.org', 'edgetunnel.anycast.eu.org', 'cdn.anycast.eu.org'];
-let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
+let proxyIP = ['pa.alighadrboland.ir']; // const proxyIPs = ['cdn-all.xn--b6gac.eu.org', 'cdn.xn--b6gac.eu.org', 'cdn-b100.xn--b6gac.eu.org', 'edgetunnel.anycast.eu.org', 'cdn.anycast.eu.org'];
 const worker_default = {
     /**
      * @param {import("@cloudflare/workers-types").Request} request
